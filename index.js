82dcf7ef4302b44e27b3106d14327744d0adb4f6
João Campos <joaog.taborda.campos@gmail.com>
2022-02-15 16:00:24 -0300

    Adiciona tratativa para os erros na leitura de arquivo

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'))
+}
+
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-    console.log(chalk.green(texto))
+  fs.readFile(caminhoDoArquivo, encoding, (error, texto) => {
+    if (error) {
+      trataErro(error)
+    } else {
+      console.log(chalk.green(texto))
+    }
   })
 }
 
