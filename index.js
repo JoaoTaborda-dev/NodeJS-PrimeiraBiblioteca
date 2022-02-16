c79ff32afde774331c5fa47b48c0345243bc39aa
João Campos <joaog.taborda.campos@gmail.com>
2022-02-16 01:34:46 -0300

    Extraindo links de um texto usando expressões regulares

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
+function extrailinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+  const arrayResultados = []
+  let temp
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] })
+  }
+  return arrayResultados
+}
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'))
 }
@@ -12,11 +22,11 @@ async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8'
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto))
+    console.log(extrailinks(texto))
   } catch (error) {
     trataErro(error)
   } finally {
-    console.log(chalk.yellow('operação concluída'))
+    console.log(chalk.blue('Operação concluída'))
   }
 }
 
